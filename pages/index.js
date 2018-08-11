df4c9959cb51855557bd71bc7543f812d0ef5804
Alexandra Rodriguez <mrodriguezv@pilandina.com.bo>
2018-08-11 11:33:34 -0400

    Actualizacion de la estructura del proyecto

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import TestComponents from "../components/TestComponents";
-
-export default class extends React.Component{
-    render(){
-        return(
-            <TestComponents></TestComponents>
-        )
-    }
-}
