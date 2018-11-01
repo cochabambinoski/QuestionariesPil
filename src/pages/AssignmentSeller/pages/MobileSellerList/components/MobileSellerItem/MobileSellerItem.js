2e5c61291617595d029bac9b3194068d8b6138be
Sergio Miranda <smirandaz@pilandina.com.bo>
2018-11-01 14:27:48 -0400

    se agrego ruta a la lista de moviles de asignación

--- src/pages/AssignmentSeller/pages/MobileSellerList/components/MobileSellerItem/MobileSellerItem.js
+++ src/pages/AssignmentSeller/pages/MobileSellerList/components/MobileSellerItem/MobileSellerItem.js
@@ -6,84 +6,91 @@ import {connect} from 'react-redux';
 import {addAssignementUser, deleteAssignementUser, editAssignementUser} from '../../../../../../actions/index';
 
 class MobileSellerItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mobileSeller: props.mobileSeller,
-            isEdit: props.isEdit,
-            initialDate: null,
-            finalDate: null,
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			mobileSeller: props.mobileSeller,
+			isEdit: props.isEdit,
+			initialDate: null,
+			finalDate: null
+		};
+	}
 
-    handleAddSeller = (seller) => {
-        this.props.handleAddSeller(seller);
-    };
+	handleAddSeller = (seller) => {
+		this.props.handleAddSeller(seller);
+	};
 
-    handleDeleteSeller = (seller) => {
-        this.props.deleteAssignement(seller);
-    };
+	handleDeleteSeller = (seller) => {
+		this.props.deleteAssignement(seller);
+	};
 
-    handleEditSeller = (seller) => {
-        this.props.editAssignementUser(seller);
-    };
+	handleEditSeller = (seller) => {
+		this.props.editAssignementUser(seller);
+	};
 
-    getDates = (seller) => {
-        const format = require('date-format');
-        const assignment = this.props.getAssignment(seller);
-        if (assignment != null) {
-            this.setState({initialDate: format("yyyy-MM-dd", new Date(assignment.initialDate))});
-            this.setState({finalDate: format("yyyy-MM-dd", new Date(assignment.finalDate))});
-        }
-    };
+	getDates = (seller) => {
+		const format = require('date-format');
+		const assignment = this.props.getAssignment(seller);
+		if (assignment != null) {
+			this.setState({initialDate: format("yyyy-MM-dd", new Date(assignment.initialDate))});
+			this.setState({finalDate: format("yyyy-MM-dd", new Date(assignment.finalDate))});
+		}
+	};
 
-    componentDidMount() {
-        this.getDates(this.props.mobileSeller);
-    }
+	componentDidMount() {
+		this.getDates(this.props.mobileSeller);
+	}
 
-    render() {
-        const {isEdit} = this.state;
-        return (
-            <div>
-                <Card className="cardMobileSeller" key={this.state.mobileSeller.id}>
-                    <div className="text">
-                        <div className="light-text">Nombre del vendedor</div>
-                        <div>{this.state.mobileSeller.vendedor.persona.nombre}</div>
-                        <div className="light-text">Tipo</div>
-                        <div>{this.state.mobileSeller.type.nombre}</div>
-                        {
-                            this.state.initialDate != null && this.state.finalDate != null ?
-                                <div>
-                                    <div className="light-text">Validez</div>
-                                    <div>Del {this.state.initialDate} al {this.state.finalDate}</div>
-                                </div> : null
-                        }
+	render() {
+		const {isEdit} = this.state;
+		return (
+			<div>
+				<Card className="cardMobileSeller" key={this.state.mobileSeller.id}>
+					<div className="text">
+						<div className="light-text">Nombre del vendedor</div>
+						<div>{this.state.mobileSeller.vendedor.persona.nombre}</div>
+						<div className="light-text">Tipo</div>
+						<div>{this.state.mobileSeller.type.nombre}</div>
+						{
+							this.state.mobileSeller.vendedor.lsrutas[0] != null ?
+								<div>
+									<div className="light-text">Ruta</div>
+									<div>{this.state.mobileSeller.vendedor.lsrutas[0].ruta.nombre}</div>
+								</div> : null
+						}
+						{
+							this.state.initialDate != null && this.state.finalDate != null ?
+								<div>
+									<div className="light-text">Validez</div>
+									<div>Del {this.state.initialDate} al {this.state.finalDate}</div>
+								</div> : null
+						}
 
-                        <br/>
-                        {
-                            isEdit === false ?
-                                <Button label="Asignar" onClick={() => {
-                                    this.handleAddSeller(this.state.mobileSeller)
-                                }}/> :
-                                <Button label="Quitar" className="ui-button-danger"
-                                        onClick={() => {
-                                            this.handleDeleteSeller(this.state.mobileSeller)
-                                        }}
-                                />
-                        }
-                    </div>
-                </Card>
-            </div>
-        );
-    }
+						<br/>
+						{
+							isEdit === false ?
+								<Button label="Asignar" onClick={() => {
+									this.handleAddSeller(this.state.mobileSeller);
+								}}/> :
+								<Button label="Quitar" className="ui-button-danger"
+								        onClick={() => {
+									        this.handleDeleteSeller(this.state.mobileSeller);
+								        }}
+								/>
+						}
+					</div>
+				</Card>
+			</div>
+		);
+	}
 }
 
 MobileSellerItem.propTypes = {};
 
 const mapDispatchToProps = dispatch => ({
-    addAssignementUser: value => dispatch(addAssignementUser(value)),
-    deleteAssignementUser: value => dispatch(deleteAssignementUser(value)),
-    editAssignementUser: value => dispatch(editAssignementUser(value))
+	addAssignementUser: value => dispatch(addAssignementUser(value)),
+	deleteAssignementUser: value => dispatch(deleteAssignementUser(value)),
+	editAssignementUser: value => dispatch(editAssignementUser(value))
 });
 
 export default connect(null, mapDispatchToProps)(MobileSellerItem);
