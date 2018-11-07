1ec076772b08fd2123fe1f485447406f5fe8d25c
Sergio Miranda <smirandaz@pilandina.com.bo>
2018-11-07 10:43:26 -0400

    cambios en vista de options

--- src/pages/Questionnaire/components/Question/FreeAnswer.js
+++ src/pages/Questionnaire/components/Question/FreeAnswer.js
@@ -6,89 +6,97 @@ import {Button} from 'primereact/button';
 import {Dropdown} from 'primereact/dropdown';
 
 class FreeAnswer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: {option: 'Sin validacion'},
-        };
-        this.addQuestion = this.addQuestion.bind(this);
-        this.updateTypeOption = this.updateTypeOption.bind(this);
-        this.addOption = this.addOption.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selected: {option: 'Sin validacion'}
+		};
+		this.addQuestion = this.addQuestion.bind(this);
+		this.updateTypeOption = this.updateTypeOption.bind(this);
+		this.addOption = this.addOption.bind(this);
+	}
 
-    validateFields(options) {
-        let emptyOptions = options.filter((option) => (option.option === ""));
-        if (emptyOptions.length === 0)
-            this.props.addQuestion();
+	validateFields(options) {
+		let emptyOptions = options.filter((option) => (option.option === ""));
+		if (emptyOptions.length === 0)
+			this.props.addQuestion();
 
-    }
+	}
 
-    addQuestion() {
-        this.props.addQuestion();
-    }
+	addQuestion() {
+		this.props.addQuestion();
+	}
 
-    addOption() {
-        this.props.addOption({
-            option: 'Sin validacion',
-            sociedadId: 'BO81',
-            usuarioId: this.props.user.username,
-            operacionId: 1,
-            fechaId: null,
-        });
-    }
+	addOption() {
+		this.props.addOption({
+			option: 'Sin validacion',
+			sociedadId: 'BO81',
+			usuarioId: this.props.user.username,
+			operacionId: 1,
+			fechaId: null
+		});
+	}
 
-    updateTypeOption(value, index) {
-        this.props.updateOption(value.option, index);
-    }
+	updateTypeOption(value, index) {
+		this.props.updateOption(value.option, index);
+	}
 
-    componentWillMount() {
-        if (this.props.lsOptions.length === 0) {
-            this.props.addOption({
-                option: 'Sin validacion',
-                sociedadId: 'BO81',
-                usuarioId: this.props.user.username,
-                operacionId: 1,
-                fechaId: null,
-            });
-        } else {
-            this.setState({selected: {option: this.props.lsOptions[0].option}});
-        }
-    }
+	componentWillMount() {
+		if (this.props.lsOptions.length === 0) {
+			this.props.addOption({
+				option: 'Sin validacion',
+				sociedadId: 'BO81',
+				usuarioId: this.props.user.username,
+				operacionId: 1,
+				fechaId: null
+			});
+		} else {
+			this.setState({selected: {option: this.props.lsOptions[0].option}});
+		}
+	}
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({selected: {option: nextProps.lsOptions[0].option}});
-    }
+	componentWillReceiveProps(nextProps) {
+		this.setState({selected: {option: nextProps.lsOptions[0].option}});
+	}
 
-    render() {
-        const validationTypes = [
-            {option: 'Sin validacion'},
-            {option: 'Texto'},
-            {option: 'Numero'},
-            {option: 'Decimal'}
-        ];
-        return (
-            <div className="ui-g" style={{width: '250px', marginBottom: '10px'}}>
-                <div>
-                    {
-                        this.props.readOnly ?
-                            <div>Tipo de validacion: {this.props.lsOptions[0].option}</div> :
-                            <Dropdown value={this.state.selected} options={validationTypes}
-                                      onChange={(e) => this.updateTypeOption(e.value, 0)} style={{width: '300px', marginBottom:'20px', marginTop:'20px'}}
-                                      placeholder="Seleccione una validacion" optionLabel="option"/>
-                    }
-                </div>
-                {
-                    this.props.readOnly ? <div/> :
-                        <div>
+	render() {
+		const validationTypes = [
+			{option: 'Sin validacion'},
+			{option: 'Texto'},
+			{option: 'Numero'},
+			{option: 'Decimal'}
+		];
+		return (
+			<div style={{marginBottom: '10px'}}>
+				<div>
+					{
+						this.props.readOnly ?
+							<div>Tipo de validacion: {this.props.lsOptions[0].option}</div> :
+							<div style={{display: 'flex'}}>
+								<Dropdown value={this.state.selected} options={validationTypes}
+								          onChange={(e) => this.updateTypeOption(e.value, 0)}
+								          style={{width: '300px', marginBottom: '20px', marginTop: '20px'}}
+								          placeholder="Seleccione una validacion" optionLabel="option"/>
+								<Dropdown value={this.state.selected} options={validationTypes}
+								          onChange={(e) => this.updateTypeOption(e.value, 0)}
+								          style={{width: '300px', marginBottom: '20px', marginTop: '20px', marginLeft: '20px'}}
+								          placeholder="Seleccione una pregunta" optionLabel="pregunta"/>
+
+							</div>
+					}
+				</div>
+				{
+					this.props.readOnly ? <div/> :
+						<div>
                             <span>
                                 <Button label="Aceptar" onClick={this.addQuestion}/>
                                 <Button label="Cancelar" onClick={this.props.handleClose} className="ui-button-danger"/>
                             </span>
-                        </div>
-                }
-            </div>
-        );
-    }
+						</div>
+				}
+			</div>
+		);
+	}
 }
 
-export default FreeAnswer
+export default FreeAnswer;
--- src/pages/Questionnaire/components/Question/MultipleOption.js
+++ src/pages/Questionnaire/components/Question/MultipleOption.js
@@ -5,114 +5,127 @@ import 'primeicons/primeicons.css';
 import {Button} from 'primereact/button';
 import {InputText} from 'primereact/inputtext';
 import {RadioButton} from 'primereact/radiobutton';
+import {Dropdown} from "primereact/dropdown";
 
 class MultipleOption extends Component {
-    constructor(props) {
-        super(props);
-        this.addQuestion = this.addQuestion.bind(this);
-        this.removeOption = this.removeOption.bind(this);
-        this.addOption = this.addOption.bind(this);
-        this.updateOption = this.updateOption.bind(this);
-        this.validateFields = this.validateFields.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.addQuestion = this.addQuestion.bind(this);
+		this.removeOption = this.removeOption.bind(this);
+		this.addOption = this.addOption.bind(this);
+		this.updateOption = this.updateOption.bind(this);
+		this.validateFields = this.validateFields.bind(this);
+	}
 
-    validateFields() {
-        if (this.props.lsOptions.length === 0) {
-            this.props.showError("Añada una opcion", "");
-            return false;
-        } else {
-            let emptyOptions = this.props.lsOptions.filter((option) => (option.option !== ""));
-            if (emptyOptions.length === 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	validateFields() {
+		if (this.props.lsOptions.length === 0) {
+			this.props.showError("Añada una opcion", "");
+			return false;
+		} else {
+			let emptyOptions = this.props.lsOptions.filter((option) => (option.option !== ""));
+			if (emptyOptions.length === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    addOption() {
-        let newOption = {
-            "id": null,
-            "question": null,
-            "option": "Opcion",
-            "sociedadId": 'BO81',
-            "usuarioId": this.props.user.username,
-            "operacionId": 1,
-            "fechaId": null,
-        };
-        this.props.addOption(newOption);
-    }
+	addOption() {
+		let newOption = {
+			"id": null,
+			"question": null,
+			"option": "Opcion",
+			"sociedadId": 'BO81',
+			"usuarioId": this.props.user.username,
+			"operacionId": 1,
+			"fechaId": null
+		};
+		this.props.addOption(newOption);
+	}
 
-    addQuestion() {
-        if (this.validateFields())
-            this.props.addQuestion();
-    }
+	addQuestion() {
+		if (this.validateFields())
+			this.props.addQuestion();
+	}
 
-    updateOption(value, index) {
-        if (this.props.assigned) {
-            if (this.props.lsOptions[index].id != null) {
-                this.props.showError("", "No se puede actualizar la opción de un cuestionario ya asignado");
-            } else {
-                this.props.updateOption(value, index);
-            }
-        } else {
-            this.props.updateOption(value, index);
-        }
-    }
+	updateOption(value, index) {
+		if (this.props.assigned) {
+			if (this.props.lsOptions[index].id != null) {
+				this.props.showError("", "No se puede actualizar la opción de un cuestionario ya asignado");
+			} else {
+				this.props.updateOption(value, index);
+			}
+		} else {
+			this.props.updateOption(value, index);
+		}
+	}
 
-    removeOption(index) {
-        if (this.props.assigned) {
-            if (this.props.lsOptions[index].id != null) {
-                this.props.showError("", "No se puede eliminar la opción de un cuestionario ya asignado");
-            } else {
-                this.props.removeOption(index);
-            }
-        } else {
-            this.props.removeOption(index);
-        }
-    }
+	removeOption(index) {
+		if (this.props.assigned) {
+			if (this.props.lsOptions[index].id != null) {
+				this.props.showError("", "No se puede eliminar la opción de un cuestionario ya asignado");
+			} else {
+				this.props.removeOption(index);
+			}
+		} else {
+			this.props.removeOption(index);
+		}
+	}
 
-    render() {
-        return (
-            <div className="ui-g" style={{width: '350px', marginBottom: '10px'}}>
-                    <div style={{paddingBottom: '15px', paddingTop: '10px', overflow:'auto', maxHeight:'150px', width: '100%',  marginBottom: '10px'}}>
-                        {
-                            this.props.lsOptions.map((option, index) => {
-                                return (
-                                    option.operacionId === 1 ?
-                                        <div style={{display: 'flex', flexDirection: 'row', marginBottom: '10px'}} key={index}>
-                                            <RadioButton value={option.option} checked={false}/>
-                                            {
-                                                this.props.readOnly ?
-                                                    <div>{option.option}</div> :
-                                                    <div>
-                                                        <InputText value={option.option}
-                                                                   onChange={(e) => this.updateOption(e.target.value, index)}/>
-                                                        <Button icon="pi pi-minus" onClick={() => {
-                                                            this.removeOption(index)
-                                                        }}/>
-                                                    </div>
-                                            }
-                                        </div> : null
-                                )
-                            })
-                        }
-                    </div>
-                <br/>
-                {
-                    this.props.readOnly ? <div></div> :
-                        <div style={{width: '400px'}}>
-                            <Button label="Añadir opcion" onClick={this.addOption} className="ui-button-secondary"/>
+	render() {
+		return (
+			<div style={{marginBottom: '10px'}}>
+				<div style={{
+					paddingBottom: '15px',
+					paddingTop: '10px',
+					overflow: 'auto',
+					maxHeight: '250px',
+					marginBottom: '10px'
+				}}>
+					{
+						this.props.lsOptions.map((option, index) => {
+							return (
+								option.operacionId === 1 ?
+									<div style={{display: 'flex', flexDirection: 'row', marginBottom: '10px'}}
+									     key={index}>
+										<RadioButton value={option.option} checked={false}/>
+										{
+											this.props.readOnly ?
+												<div>{option.option}</div> :
+												<div>
+													<InputText value={option.option}
+													           onChange={(e) => this.updateOption(e.target.value, index)}/>
+													<Dropdown value={null}
+													          onChange={(e) => this.updateTypeOption(e.value, 0)}
+													          style={{width: '300px', marginLeft: '20px'}}
+													          placeholder="Seleccione una pregunta"
+													          optionLabel="pregunta"/>
+													<Button icon="pi pi-minus" onClick={() => {
+														this.removeOption(index);
+													}}/>
+												</div>
+										}
+									</div> : null
+							);
+						})
+					}
+				</div>
+				<br/>
+				{
+					this.props.readOnly ? <div></div> :
+						<div style={{width: '400px'}}>
+							<Button label="Añadir opcion" onClick={this.addOption} className="ui-button-secondary"/>
 
 
-                            <span>
+							<span>
                                 <Button label="Aceptar" onClick={this.addQuestion}/>
                                 <Button label="Cancelar" onClick={this.props.handleClose} className="ui-button-danger"/>
                             </span>
-                        </div>
-                }
-            </div>
-        );
-    }
+						</div>
+				}
+			</div>
+		);
+	}
 }
 
-export default MultipleOption
+export default MultipleOption;
