5cc140764dbc52b49ed67889b3e6837a6a286c8a
Brian Dennis Vega Hidalgo <bvega@pilandina.com.bo>
2019-08-02 15:55:42 -0400

    refactorizacion de la estuctura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,22 @@
-import React, { Component } from 'react';
-import 'primereact/resources/themes/nova-dark/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import { Route, Switch } from 'react-router-dom';
-import QuestionnairesList from './pages/QuestionnairesList/pages/QuestionnairesList.jsx';
-import Questionnaire from './pages/Questionnaire/pages/Questionnaire/Questionnaire.jsx'
-
-const QuuestionnairesList = () => (
-  <div>
-    <QuestionnairesList />
-  </div>
-);
-
-const Quuestionnaire = () => (
-  <div>
-    <Questionnaire />
-  </div>
-);
+import React, {Component} from 'react';
+import {themePrimary} from "./styles/Themes";
+import {Provider} from "react-redux";
+import {store} from "./store";
+import InitialPage from "./pages/InitialPage/InitialPage";
+import {MuiThemeProvider} from "@material-ui/core";
 
 class App extends Component {
-  state = {
-    questionnaire: {},
-    toQuestionnaire: false,
-  };
-
-  goToQuestionnaire = (questionnaire) => {
-    this.setState({
-      questionnaire: questionnaire,
-      toQuestionnaire: true,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-         <Switch className="App">
-          <Route path="/questionnaires" exact component={QuuestionnairesList} />
-          <Route path="/questionnaires/new" exact component={Quuestionnaire} />
-          <Route path="/questionnaires/:id" exact render={(props) => <Questionnaire {...props} readOnly={true}></Questionnaire>}  />
-          <Route path="/questionnaires/edit/:id" exact render={(props) => <Questionnaire {...props} readOnly={false}></Questionnaire>}  />
-        </Switch>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider theme={themePrimary}>
+                    <Provider store={store}>
+                        <InitialPage/>
+                    </Provider>
+                </MuiThemeProvider>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/layout/layout.css'
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
-import {store} from './store/index'
-import InitialPage from "./pages/InitialPage/InitialPage";
-import { MuiThemeProvider} from '@material-ui/core/styles';
-import {themePrimary} from './styles/Themes';
+import App from "./App";
 
-ReactDOM.render(
-    <MuiThemeProvider theme={themePrimary}>
-        <Provider store={store}>
-            <InitialPage/>
-        </Provider>
-    </MuiThemeProvider>
-    , document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
